06af988b6a5563861b45bfa51d2d7c0f89fc3e33
Umesh Shrestha <moru@mac.local>
2022-04-27 20:26:25 +0545

    vscode lunch config

--- lib/config/enviromet_variables/enviroment_variables.dart
+++ lib/config/enviromet_variables/enviroment_variables.dart
@@ -1,9 +1,6 @@
 class Env {
   /// the variable to define which server is to use
   /// can be *dev, test or prod
-  static const String servermode =
-      String.fromEnvironment('build', defaultValue: 'dev');
-
-  static const String apiKey = String.fromEnvironment('apiKey',
-      defaultValue: '08e7fca767ba444e93de66b948606021');
+  static const String servermode = String.fromEnvironment('build');
+  static const String apiKey = String.fromEnvironment('apiKey');
 }
